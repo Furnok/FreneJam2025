e48c8f254bfee793ee16959ea0579d7d9bc03319
Furnok <killian.furno@orange.fr>
2025-04-10 01:05:50 +0200

    Fix: Reset

--- Assets/App/Scripts/Managers/S_InputsManager.cs
+++ Assets/App/Scripts/Managers/S_InputsManager.cs
@@ -4,12 +4,29 @@ using UnityEngine.InputSystem;
 public class S_InputsManager : MonoBehaviour
 {
     [Header("Input")]
-    [SerializeField] private RSE_Menu rseMenu;
+    
+    [SerializeField] private RSE_Reset rseReset;
 
     [Header("Output")]
+    [SerializeField] private RSE_Menu rseMenu;
     [SerializeField] private SSO_DelayInputs ssoDelayInputs;
     [SerializeField] private RSE_Move rseMove;
     [SerializeField] private RSE_Interraction rseInterraction;
+
+    private void OnEnable()
+    {
+        rseReset.action += ResetScript;
+    }
+
+    private void OnDisable()
+    {
+        rseReset.action -= ResetScript;
+    }
+
+    private void ResetScript()
+    {
+        StopAllCoroutines();
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
